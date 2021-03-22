efccf6a079aad7f5c27abc2f6496c327e7e94772
Murilo Beltrame <murilobeltrame@gmail.com>
2021-03-22 01:43:40 -0300

    updating application tests + stryker configuration

--- src/DemoBookStore.Application.Unit.Tests/Books/Commands/CreateBookCommandTests.cs
+++ src/DemoBookStore.Application.Unit.Tests/Books/Commands/CreateBookCommandTests.cs
@@ -1,14 +1,43 @@
 using DemoBookStore.Application.Books.Commands.CreateBook;
+using DemoBookStore.Application.Common.Interfaces;
+using DemoBookStore.Domain.Entities;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace DemoBookStore.Application.Unit.Tests
 {
     public class CreateBookCommandTests
     {
+        private readonly CancellationToken _cancelationToken = new();
+
         [Fact]
-        public void CreateBookCommand_should_be_instantiated()
+        public async Task CreateBookCommand_should_be_instantiated()
         {
-            var createBookCommand = new CreateBookCommand();
+            var repositoryMock = new Mock<IRepository<Book>>();
+            var createBookCommandHandler = new CreateBookCommandHandler(repositoryMock.Object);
+            var createBookCommand = new CreateBookCommand{
+                Title = "A book",
+                Authors = new List<AuthorDto> { new AuthorDto{ FirstName = "Name", LastName = "LastName"}},
+                PublisherName = "The Publisher"
+            };
+            repositoryMock.Setup(repository => repository.CreateAsync(createBookCommand.ToBook(), _cancelationToken).Result).Returns(createBookCommand.ToBook());
+
+            var responseExpected = new CreateBookResponse(
+                createBookCommand.Title,
+                createBookCommand.Authors.Select(authorDto => new Author(authorDto.FirstName, authorDto.LastName)),
+                new Publisher(createBookCommand.PublisherName),
+                createBookCommand.Pages,
+                0m
+            );
+
+
+            var response = await createBookCommandHandler.Handle(createBookCommand, _cancelationToken);
+
+            Assert.Equal(responseExpected, response);
         }
     }
 }
